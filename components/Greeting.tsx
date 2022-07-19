1968474d4afe05a3b1299d1616eba37900072c77
Patrick-web <pntxall100@gmail.com>
2022-07-19 14:53:52 +0300

    Removed default username

--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -1,39 +1,39 @@
-import React, { useContext, useEffect, useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { GlobalContext } from "../context/globalContext";
+import React, {useContext, useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {GlobalContext} from '../context/globalContext';
 
-const Greeting = ({ isListening: isListening }: { isListening: boolean }) => {
+const Greeting = ({isListening: isListening}: {isListening: boolean}) => {
   const [greeting, setGreeting] = useState({
-    timeOfDay: "Unknown",
-    image: require("../assets/galaxy-icon.png"),
-    text: `Hey Patrick`,
+    timeOfDay: 'Unknown',
+    image: require('../assets/galaxy-icon.png'),
+    text: `Hi `,
   });
-  const { chats } = useContext(GlobalContext);
+  const {chats} = useContext(GlobalContext);
   useEffect(() => {
     const hourOfTheDay = new Date().getHours();
     if (hourOfTheDay > 4 && hourOfTheDay < 12) {
       setGreeting({
-        timeOfDay: "Morning",
-        image: require("../assets/morning-icon.png"),
-        text: "Good Morning",
+        timeOfDay: 'Morning',
+        image: require('../assets/morning-icon.png'),
+        text: 'Good Morning',
       });
     } else if (hourOfTheDay > 12 && hourOfTheDay < 17) {
       setGreeting({
-        timeOfDay: "Afternoon",
-        image: require("../assets/afternoon-icon.png"),
-        text: "Good Afternoon",
+        timeOfDay: 'Afternoon',
+        image: require('../assets/afternoon-icon.png'),
+        text: 'Good Afternoon',
       });
     } else if (hourOfTheDay > 17 && hourOfTheDay < 20) {
       setGreeting({
-        timeOfDay: "Evening",
-        image: require("../assets/sunset-icon.png"),
-        text: "Good Evening",
+        timeOfDay: 'Evening',
+        image: require('../assets/sunset-icon.png'),
+        text: 'Good Evening',
       });
     } else if (hourOfTheDay > 19 && hourOfTheDay < 25) {
       setGreeting({
-        timeOfDay: "Night",
-        image: require("../assets/night-icon.png"),
-        text: "A Beautiful Night",
+        timeOfDay: 'Night',
+        image: require('../assets/night-icon.png'),
+        text: 'A Beautiful Night',
       });
     }
   }, []);
@@ -47,8 +47,8 @@ const Greeting = ({ isListening: isListening }: { isListening: boolean }) => {
       <View
         style={{
           ...styles.greeting,
-          flexDirection: chats.length > 1 ? "row" : "column",
-          justifyContent: chats.length > 1 ? "flex-start" : "space-between",
+          flexDirection: chats.length > 1 ? 'row' : 'column',
+          justifyContent: chats.length > 1 ? 'flex-start' : 'space-between',
           height: chats.length > 1 ? 60 : 200,
         }}>
         <Image
@@ -74,12 +74,12 @@ const Greeting = ({ isListening: isListening }: { isListening: boolean }) => {
 
 const styles = StyleSheet.create({
   greeting: {
-    alignItems: "center",
-    width: "100%",
+    alignItems: 'center',
+    width: '100%',
   },
   greetingText: {
-    color: "white",
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
   },
   greetingImage: {
     width: 150,
